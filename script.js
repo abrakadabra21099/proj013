c90be47f0923eb1c9f7748ba63dbc16f74734d4b
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-04-08 16:04:31 +0400

    Learn 37. Done.

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 'use strict';
 
-const btns = document.querySelectorAll('button');
-console.log(btns[0].classList.length);
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// btns[0].classList.add('red', 'dhsfkjh');
+// btns[1].classList.add('red');
+// btns[0].classList.remove('blue');
+// btns[0].classList.toggle('blue');
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red')
+    // } else {
+    //     btns[1].classList.remove('red')
+    // }
+    btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (e) => {
+    // console.dir(e.target);
+    // if (e.target && e.target.tagName == 'BUTTON') {
+    if (e.target && e.target.matches('button.red')) {
+            // if (e.target && e.target.classList.contains('some')) {
+        console.log('hell')
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.appendChild(btn);
